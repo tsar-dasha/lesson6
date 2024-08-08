d8880423565362f2473fcfd99e1df3c862029232
Tsareva Darya <d.tsareva@space307.com>
2024-08-08 11:21:17 +0200

    added fix with day

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -4,8 +4,14 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class CalendarComponent {
     public void setDate(String day, String month, String year) {
+        int dayNumber = Integer.parseInt(day);
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
-        $(".react-datepicker__day--00" + day + ":not(.react-datepicker__day--outside-month)").click();
+        if (dayNumber < 10) {
+            $(".react-datepicker__day--0" + day).click();
+        }
+        else {
+            $(".react-datepicker__day--" + day).click();
+        }
     }
 }
